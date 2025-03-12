4e6e86e9df162c1a3bf58b436a1fd551583c79bf
Dasia <dasiapitts@yahoo.com>
2025-03-12 13:52:37 -0400

    Initial commit with Python scripts

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("Addition:", sum_result)
+print("Subtraction:", difference)
+print("Multiplication:", product)
+print("Division:", quotient)
+
